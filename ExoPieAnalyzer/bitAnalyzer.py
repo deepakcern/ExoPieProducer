d585f7423d13563b66993e75f73a80849ddf8fae
Raman Khurana <raman.khurana@cern.ch>
2019-10-18 15:00:48 +0200

    adding bit wise operation example, need a decoder before we start using it

--- ExoPieAnalyzer/bitAnalyzer.py
+++ ExoPieAnalyzer/bitAnalyzer.py
@@ -1,13 +1,13 @@
-bits=[0,1,2,3,5]
+bits=[1,1,1,1,0,1,0]
 
 x=1 ## always 1
 y=0
 z=0
 
-for ibit in bits:
-    y = x << ibit
+for ibit in range(len(bits)):
+    y = bits[ibit] << ibit
     z = z ^ y
-    print ibit, y, z
+    print bits[ibit], y, z
 
     ## now based on the value of z we can decide which cuts were passed. 
     #print ibit
