7594cf55b14c12edfb1467ce8e8b67834729c324
Raman Khurana <raman.khurana@cern.ch>
2019-10-18 15:56:11 +0200

    an exmaple opf histogram also added

--- ExoPieAnalyzer/bitAnalyzer.py
+++ ExoPieAnalyzer/bitAnalyzer.py
@@ -1,3 +1,5 @@
+from ROOT import TH1F, TFile 
+
 bits=[1,1,1,1,0,1,0]
 
 x=1 ## always 1
@@ -24,12 +26,19 @@ def setBits(n):
         z = z ^ y
         
     return z
-        
+
+h = TH1F("h_cutflow", "h_cutflow", 10,0,10)        
 for i in [1,2,3,4,5,6]:
     
     print i, setBits(i), setBits(i) & z 
     
     if setBits(i) == (setBits(i) & z) :
         print ("all selection upto ",i," passed")
-        
+        h.AddBinContent(i, 1)
+
+f_out = TFile("test.root", "RECREATE")
+f_out.cd()
+h.Write()
+f_out.Close()
+
         ## for those cuts, when this print statement is print on screen will be added in the cutflow histogram 
