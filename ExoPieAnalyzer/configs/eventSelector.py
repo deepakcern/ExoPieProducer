f23beef913b6d6c43396e93c808f8b6948f758a2
Deepak Kumar <dekumar@Deepaks-iMac.local>
2020-02-06 17:46:03 +0530

    added photon veto for resolved category

--- ExoPieAnalyzer/configs/eventSelector.py
+++ ExoPieAnalyzer/configs/eventSelector.py
@@ -55,7 +55,7 @@ def getSel_resolved(nEle,nTightEle,isTightEle,nMu,nTightMu,isTightMuon,nTau,nPho
     #if nak4jets>1: jetCond = ak4CSV[0] > MWP and ak4CSV[1] > MWP and ak4pt[0] > 50 and (abs(ak4eta[0]) < 2.5)
 
 
-    baseline       = nak4jets >= 2 and nTau==0  #and h_mass > 100.0 and h_mass < 150.0 # and nPho==0
+    baseline       = nak4jets >= 2 and nTau==0  and nPho==0 #and h_mass > 100.0 and h_mass < 150.0 # and nPho==0
    
     cuts['resolved_signal'] = baseline and nMu+nEle==0 and pfMet > 200.0 and h_mass > 100.0 and h_mass < 150.0 and mini_ak4jet_MET_dPhi > 0.4
     cuts['resolved_SBand'] = baseline and nMu+nEle==0 and pfMet > 200.0 and ((h_mass > 50.0 and h_mass < 100.0) or (h_mass > 150.0 and h_mass < 350.0)) and mini_ak4jet_MET_dPhi > 0.4
