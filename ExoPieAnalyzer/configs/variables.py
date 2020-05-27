cbe67824f4534d714bc9c7fe047573a05c64f923
Praveen Chandra Tiwari <tiwariPC@users.noreply.github.com>
2020-05-27 12:00:26 +0530

    addid prefiring weight for 2016 and 2017

--- ExoPieAnalyzer/configs/variables.py
+++ ExoPieAnalyzer/configs/variables.py
@@ -1,4 +1,5 @@
 allvars_bbDM=['st_runId', 'st_lumiSection', 'st_eventId', \
+'st_prefiringweight','st_prefiringweightup','st_prefiringweightdown',\
 'st_pfMetCorrPt', 'st_pfMetCorrPhi', 'st_pfMetUncJetResUp', 'st_pfMetUncJetResDown', 'st_pfMetUncJetEnUp', 'st_pfMetUncJetEnDown', \
 'WenuPhi', 'WmunuPhi', 'ZeePhi', 'ZmumuPhi', \
 'ZeeRecoil', 'ZmumuRecoil', 'WenuRecoil', 'WmunuRecoil', \
