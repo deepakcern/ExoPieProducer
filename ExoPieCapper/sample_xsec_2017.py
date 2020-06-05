de37f98e72ddfb05e4bac25b2cc451225cb509d0
Praveen Chandra Tiwari <tiwariPC@users.noreply.github.com>
2020-06-05 17:41:42 +0530

    updated the crossections

--- ExoPieCapper/sample_xsec_2017.py
+++ ExoPieCapper/sample_xsec_2017.py
@@ -9,18 +9,19 @@ def getXsec(samplename):
     if 'DYJetsToLL_M-50_HT-600to800'   in samplename: Xsec  = 1.747000
     if 'DYJetsToLL_M-50_HT-800to1200'   in samplename: Xsec  = 0.805200
 
-    if 'GJets_HT-100To200'   in samplename: Xsec  = 8622.000000
+    if 'GJets_HT-40To100'   in samplename: Xsec  = 20790.000000
+    if 'GJets_HT-100To200'   in samplename: Xsec  = 9238.000000
     if 'GJets_HT-200To400'   in samplename: Xsec  = 2193.000000
-    if 'GJets_HT-40To100'   in samplename: Xsec  = 18620.000000
-    if 'GJets_HT-600ToInf'   in samplename: Xsec  = 85.210000
+    if 'GJets_HT-400To600'   in samplename: Xsec  = 274.400000
+    if 'GJets_HT-600ToInf'   in samplename: Xsec  = 93.460000
 
-    if 'QCD_HT1000to1500'   in samplename: Xsec  = 1094.000000
-    if 'QCD_HT1500to2000'   in samplename: Xsec  = 99.010000
-    if 'QCD_HT2000toInf'   in samplename: Xsec  = 20.230000
     if 'QCD_HT200to300'   in samplename: Xsec  = 1556000.000000
     if 'QCD_HT300to500'   in samplename: Xsec  = 323600.000000
-    if 'QCD_HT500to700'   in samplename: Xsec  = 29990.000000
-    if 'QCD_HT700to1000'   in samplename: Xsec  = 6351.000000
+    if 'QCD_HT500to700'   in samplename: Xsec  = 32100.000000
+    if 'QCD_HT700to1000'   in samplename: Xsec  = 6831.000000
+    if 'QCD_HT1000to1500'   in samplename: Xsec  = 1207.000000
+    if 'QCD_HT1500to2000'   in samplename: Xsec  = 119.900000
+    if 'QCD_HT2000toInf'   in samplename: Xsec  = 25.240000
 
     if 'ST_s-channel_4f_leptonDecays'   in samplename: Xsec  = 3.740000
     if 'ST_t-channel_antitop_4f_inclusiveDecays'   in samplename: Xsec  = 67.910000
@@ -33,23 +34,23 @@ def getXsec(samplename):
     if 'TTToSemiLeptonic_TuneCP5'   in samplename: Xsec  = 687.100000*0.438
 
     if 'WJetsToLNu_HT-100To200'   in samplename: Xsec  = 1395.000000
-    if 'WJetsToLNu_HT-1200To2500'   in samplename: Xsec  = 1.080000
-    if 'WJetsToLNu_HT-200To400'   in samplename: Xsec  = 409.300000
-    if 'WJetsToLNu_HT-2500ToInf'   in samplename: Xsec  = 0.008053
-    if 'WJetsToLNu_HT-400To600'   in samplename: Xsec  = 57.910000
-    if 'WJetsToLNu_HT-600To800'   in samplename: Xsec  = 12.930000
-    if 'WJetsToLNu_HT-800To1200'   in samplename: Xsec  = 5.395000
+    if 'WJetsToLNu_HT-200To400'   in samplename: Xsec  = 407.900000
+    if 'WJetsToLNu_HT-400To600'   in samplename: Xsec  = 57.480000
+    if 'WJetsToLNu_HT-600To800'   in samplename: Xsec  = 12.870000
+    if 'WJetsToLNu_HT-800To1200'   in samplename: Xsec  = 5.366000
+    if 'WJetsToLNu_HT-1200To2500'   in samplename: Xsec  = 1.074000
+    if 'WJetsToLNu_HT-2500ToInf'   in samplename: Xsec  = 0.008001
 
     if 'WW_TuneCP5_13TeV-'   in samplename: Xsec  = 75.900000
     if 'WZ_TuneCP5_13TeV-'   in samplename: Xsec  = 27.570000
 
-    if 'ZJetsToNuNu_HT-100To200'   in samplename: Xsec  = 304.500000
-    if 'ZJetsToNuNu_HT-1200To2500'   in samplename: Xsec  = 0.343000
-    if 'ZJetsToNuNu_HT-200To400'   in samplename: Xsec  = 91.850000
-    if 'ZJetsToNuNu_HT-2500ToInf'   in samplename: Xsec  = 0.005146
-    if 'ZJetsToNuNu_HT-400To600'   in samplename: Xsec  = 13.110000
-    if 'ZJetsToNuNu_HT-600To800'   in samplename: Xsec  = 3.257000
-    if 'ZJetsToNuNu_HT-800To1200'   in samplename: Xsec  = 1.496000
+    if 'ZJetsToNuNu_HT-100To200'   in samplename: Xsec  = 280.35
+    if 'ZJetsToNuNu_HT-200To400'   in samplename: Xsec  = 77.67
+    if 'ZJetsToNuNu_HT-400To600'   in samplename: Xsec  = 10.73
+    if 'ZJetsToNuNu_HT-600To800'   in samplename: Xsec  = 2.559
+    if 'ZJetsToNuNu_HT-800To1200'   in samplename: Xsec  = 1.1796
+    if 'ZJetsToNuNu_HT-1200To2500'   in samplename: Xsec  = 0.28833
+    if 'ZJetsToNuNu_HT-2500ToInf'   in samplename: Xsec  = 0.006945
 
     if 'ZZ_TuneCP5'   in samplename: Xsec  = 12.140000
 
@@ -59,6 +60,4 @@ def getXsec(samplename):
     if 'WplusH_HToBB_WToLNu_M125' in samplename: Xsec = 0.2819
     if 'ZH_HToBB_ZToLL_M125'      in samplename and 'ggZH_HToBB_ZToLL_M125' not in samplename:   Xsec = 0.07924
     if 'ZH_HToBB_ZToNuNu_M125'    in samplename and 'ggZH_HToBB_ZToNuNu_M125' not in samplename: Xsec = 0.1565
-
-
     return Xsec
