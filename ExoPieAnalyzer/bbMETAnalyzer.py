4c4f2ee4252e5ad5e694b0284daa34cc19c04794
Praveen Chandra Tiwari <physics.pctiwari@gmail.com>
2020-06-25 16:44:33 +0530

    minor fix

--- ExoPieAnalyzer/bbMETAnalyzer.py
+++ ExoPieAnalyzer/bbMETAnalyzer.py
@@ -493,7 +493,7 @@ def runbbdm(txtfile):
                 weightPrefire_down = ep_prefiringweightdown
                 common_weight = weightB * weightEWK * weightQCD * weightTop * weightPU * weightPrefire
                 presel_weight = weightEWK * weightQCD * weightTop * weightPU * weightPrefire
-                weight,weightEleTrig,ele_wgt,mu_wgt,recoil_wgt,met_wgt = weight_(common_weight,ep_pfMetCorrPt,ep_ZmumuRecoil,ep_WmunuRecoil,ep_nEle_index,ep_elePt,ep_eleEta,ep_nMu,ep_muPt,ep_muEta)
+                weight,weightEleTrig,ele_wgt,mu_wgt,recoil_wgt,met_wgt = weight_(common_weight,ep_pfMetCorrPt,ep_ZmumuRecoil,ep_WmunuRecoil,ep_nEle_index,ep_elePt,ep_eleEta,ep_eleIsPTight,ep_nMu,ep_muPt,ep_muEta,ep_isTightMuon)
                 weightEle=ele_wgt[0];weightMu=mu_wgt[0];weightRecoil=recoil_wgt[0];weightMET=met_wgt[0]
                 weightEle_up=ele_wgt[1];weightMu_up=mu_wgt[1];weightRecoil_up=recoil_wgt[1];weightMET_up=met_wgt[1]
                 weightEle_down=ele_wgt[2];weightMu_down=mu_wgt[2];weightRecoil_down=recoil_wgt[2];weightMET_down=met_wgt[2]
